e63370a6eb4365f3d7bbc3a58cd93dc4761c3ee4
Antonio Gaquit <113430868+agaquit@users.noreply.github.com>
2024-01-21 16:55:35 +0100

    Add files via upload

---
+++ Portfolio/Controllers/PortfolioController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Portfolio.NewFolder;
+
+
+namespace Portfolio.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("ValidAzure")]
+    public class PortfolioController : ControllerBase
+    {
+        private static List<PortfolioList> heroes = new List<PortfolioList>
+        {
+            new PortfolioList
+            {
+                ProjektName ="BankApp", Link = "https://github.com/CarlVictorGrimschold/GrimscholdsBank", Stack= "C#",
+                Datum="2023-05-25", Beskrivning="Appen är utvecklad i C# och är under vidareutveckling"},
+            new PortfolioList
+            {
+                ProjektName ="Webbshopen", Link = "https://github.com/CarlVictorGrimschold/Webbshoppen-Victor", Stack= "HTML,Javascript,CSS",
+                Datum="2023-03-01", Beskrivning="En webbshop utvecklad med React där HTML, Javascript används, och Metoden SaSS för reglera CSS"},
+            new PortfolioList
+            {
+                ProjektName ="", Link = "https://github.com/CarlVictorGrimschold/2023PorfolioVictor", Stack= "HTML,Javascript,CSS,C#",
+                Datum="2023-06-18", Beskrivning="En hemsida som hanterar ett portfolio innehållandes ett kontaktformulör api"},
+            new PortfolioList
+            {
+                ProjektName ="HotellApp", Link = "https://github.com/CarlVictorGrimschold/VictorHotell2023", Stack= "C#",
+                Datum="2023-01-04", Beskrivning="En consolapp som hantera bokningar skriven med C#"},
+        };
+
+        [HttpGet]
+        public async Task<ActionResult<List<PortfolioList>>> GetAll()
+        {
+            return Ok(heroes);
+        }
+    }
+}
---
+++ Portfolio/NewFolder/PortfolioList.cs
@@ -0,0 +1,12 @@
+﻿namespace Portfolio.NewFolder
+{
+    public class PortfolioList
+    {
+        public string ProjektName { get; set; } = string.Empty;
+        public string Link { get; set; } = string.Empty;
+        public string Stack { get; set; } = string.Empty;
+        public string Datum { get; set; } = string.Empty;
+        public string Beskrivning { get; set; } = string.Empty;
+
+    }
+}
